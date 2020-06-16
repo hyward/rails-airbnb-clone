7f9fedef6e9cf2c3e16973a08c79e40e8cd06865
Hassanelashram <elashramhassan@gmail.com>
2020-06-16 12:36:17 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+100.times do
+  make = Faker::Vehicle.make
+  @car = Car.new(
+    name: Faker::Vehicle.model(make_of_model: make),
+    brand: make,
+    transmission: Faker::Vehicle.transmission,
+    price: Faker::Vehicle.mileage(min: 100, max: 1000),
+    seats: Faker::Vehicle.door_count,
+    trunk: Faker::Vehicle.mileage(min: 100, max: 1000),
+    )
+  @car.save
+end
