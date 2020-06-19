b203e4de12c386b5a1894789b6b85e0242820572
zakariae el haniti <zikoiliass@gmail.com>
2020-06-19 15:23:29 +0200

    authentication

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
   validates :status, inclusion: { in: ['pending', 'rejected', 'confirmed']}
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
