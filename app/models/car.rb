c7850830e2282b328d90d15b7ad163770fa0f37d
Kasimir von Halem <kasimir.halem@me.com>
2020-06-19 14:16:58 +0200

    validations done

--- app/models/car.rb
+++ app/models/car.rb
@@ -14,4 +14,5 @@ class Car < ApplicationRecord
   validates :city, presence: true
   validates :user, presence: true
   validates :address, presence: true
+  validates :description, presence: true
 end
