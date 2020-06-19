16d5c3cb387267f857599d069c089d059f92ff97
Kasimir von Halem <kasimir.halem@me.com>
2020-06-19 12:45:07 +0200

    validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,4 +6,12 @@ class Car < ApplicationRecord
   has_many_attached :photos
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  validates :name, presence: true
+  validates :brand, presence: true
+  validates :seats, presence: true
+  validates :transmission, presence: true
+  validates :trunk, presence: true
+  validates :city, presence: true
+  validates :user, presence: true
+  validates :address, presence: true
 end
