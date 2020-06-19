0809265ef6985627b1a55fde3a6081b85f57a3af
Hassanelashram <elashramhassan@gmail.com>
2020-06-19 12:30:45 +0200

    avatar added to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :name, :photo])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :cars
   has_many :favorites
   has_many :bookings
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
